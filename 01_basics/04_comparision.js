9c81041d30a64bef77cfc3066405595759889699
Ashvin Singh <136104771+ashvin-to@users.noreply.github.com>
2024-10-08 18:12:13 +0530

    data comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// it is not a good practice to compare the two different data types.
+// before comparsion make sure that youre comparing same data types.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// above comparision can lead to the confusion 
+
+// === 
+// it also checks the data type
+
+console.log("2" === 2);
